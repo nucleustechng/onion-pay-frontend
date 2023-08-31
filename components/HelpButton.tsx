952ec8eb2cb4d5ddaf9525503f15534ed466abff
JosephKeswet <josephkeswet@gmail.com>
2023-08-31 18:31:17 +0100

    chore:removed help button temporarily

--- components/HelpButton.tsx
+++ components/HelpButton.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-
+// import { useRouter } from "next/router";
+import React from "react";
 
 const HelpButton = () => {
-  return (
-    <div>
-        <button className='bg-[#3063E9] rounded-[0.32rem] w-[10rem] md:w-[10rem] xl:w-[10rem]  text-sm lg:w-[11.15rem] h-11 lg:text-base text-white font-WorkSans font-normal leading-4'>
-            Do you need help?
-        </button>
-    </div>
-  )
-}
+	// const router = useRouter();
+	return (
+		<div>
+			{/* <button
+				onClick={() => router.push("/contact")}
+				className="bg-[#3063E9] rounded-[0.32rem] w-[10rem] md:w-[10rem] xl:w-[10rem]  text-sm lg:w-[11.15rem] h-11 lg:text-base text-white font-WorkSans font-normal leading-4"
+			>
+				Do you need help?
+			</button> */}
+		</div>
+	);
+};
 
-export default HelpButton
+export default HelpButton;
