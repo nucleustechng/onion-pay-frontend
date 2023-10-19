64c35c45dbd6d850672ae20b7a1562d2da7c6ed2
JosephKeswet <josephkeswet@gmail.com>
2023-10-19 10:08:24 +0100

    feat:route to dashboard on site visit if token present

---
+++ components/termsData.ts
@@ -0,0 +1,47 @@
+export const paymentProtectionPromiseContent = [
+	{
+		title: "Secure Transactions:",
+		description:
+			"We utilize advanced encryption and security protocols to safeguard payment transactions made through Onion Pay. Your financial information is protected throughout the payment process.",
+	},
+	{
+		title: "Fraud Detection and Prevention:",
+		description:
+			"We employ sophisticated fraud detection tools and strategies to identify and prevent fraudulent transactions. Suspicious activities are promptly flagged and investigated to protect both merchants and customers.",
+	},
+	{
+		title: "Data Protection:",
+		description:
+			"We adhere to strict data protection standards and comply with applicable data privacy laws. Your personal and financial information is handled with care and confidentiality.",
+	},
+	{
+		title: "Compliance with Regulations:",
+		description:
+			"Onion Pay complies with all relevant regulations and industry standards in Nigeria to ensure that our payment gateway operates in accordance with the law.",
+	},
+	{
+		title: "Customer Support:",
+		description:
+			"Our dedicated customer support team is available to assist you with any questions, concerns, or issues related to your Onion Pay transactions. We strive to provide timely and effective support to ensure a smooth payment experience.",
+	},
+	{
+		title: "Transparent Fees:",
+		description:
+			"We are committed to transparency in our fee structure. All applicable fees for using Onion Pay will be clearly communicated, so you know exactly what to expect.",
+	},
+	{
+		title: "Timely Remittance:",
+		description:
+			"Merchants can expect timely remittance of collected funds to their designated bank accounts. We aim to ensure that your business operations are not hindered by payment delays.",
+	},
+	{
+		title: "Continuous Improvement:",
+		description:
+			"We are dedicated to continuous improvement and invest in the enhancement of Onion Pay's security features, user experience, and overall reliability.",
+	},
+	{
+		title: "Contact Us:",
+		description:
+			"If you have any questions, concerns, or feedback regarding our Payment Protection Promise or the use of Onion Pay, please contact us at [contact email/phone]. We value your trust and are here to assist you.",
+	},
+];
