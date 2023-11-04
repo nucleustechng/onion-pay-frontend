dadcce8756bbda57ef57d8251ab34277abe97645
JosephKeswet <josephkeswet@gmail.com>
2023-11-04 16:04:41 +0100

    fix:wrong url

---
+++ modules/PaymentPageApi/paymenyPageService.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const token = Cookies.get("token");
+
+export const getPaymentPages = async () => {
+	const { data } = await axios.get(
+		`${process.env.NEXT_PUBLIC_URL}/api/v1/payment-pages`,
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}
+	);
+	return data?.pages;
+};
