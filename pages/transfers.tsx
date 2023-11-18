3ac7636d15e1dd0e03acd309a4f3b4d36794e1fa
JosephKeswet <josephkeswet@gmail.com>
2023-11-18 13:37:32 +0100

    feat:transactions pagination

--- pages/transfers.tsx
+++ pages/transfers.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import HelpButton from '../components/HelpButton'
+import React from "react";
+import HelpButton from "../components/HelpButton";
 // import TransfersEmpty from '../../components/payments/TransfersEmpty'
 // import PaymentLinks from '../../components/payments/PaymentLinks'
 // import PaymentPlans from '../../components/payments/payment plans/PaymentPlans'
-import TransfersEmpty from '../components/payments/TransfersEmpty'
-import useAuth from '../useAuth'
-
+import TransfersEmpty from "../components/payments/TransfersEmpty";
+import useAuth from "../useAuth";
 
 const Transfers = () => {
-  useAuth()
- 
-  return (
-    <div>
-        <div>
-            <div>
-                <TransfersEmpty/>
-            </div>
-            <div className='fixed left-auto top-2/3 right-0 mr-7 z-30 mt-[8.5rem]'>
-                    <HelpButton/>
-                </div>
-           
-        </div>
-    </div>
-  )
-}
+	useAuth();
 
-export default Transfers
+	return (
+		<div>
+			<div>
+				<div  className="flex-1">
+					<TransfersEmpty />
+				</div>
+				<div className="fixed left-auto top-2/3 right-0 mr-7 z-30 mt-[8.5rem]">
+					<HelpButton />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Transfers;
