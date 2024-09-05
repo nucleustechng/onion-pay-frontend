8459133b9c838857103c4915ebad7bafc29986b2
JosephKeswet <josephkeswet@gmail.com>
2024-09-05 14:16:12 +0100

    (FIX):verify popup

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Sidebar from "../components/new/Sidebar";
@@ -9,27 +9,10 @@ import { ToastContainer } from "react-toastify";
 // import SideBar from "../components/SideBar";
 // import SideBarMobile from "../components/SidebarMobile";
 import "react-toastify/dist/ReactToastify.css";
-import { useSetting } from "../modules/services/useSetting";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import VerifyEmailModal from "../components/settings/VerifyEmailModal";
 
 const Layout = ({ children }: any) => {
-  const { getSettings } = useSetting();
   const router = useRouter();
-  const [showModal, setShowModal] = useState<boolean>(true);
-
-  const { data } = useQuery({
-    queryKey: ["settings"],
-    queryFn: () => getSettings(),
-  });
-  const verified = data?.merchant?.verified;
-
-  useEffect(() => {
-    if (verified === false) {
-      setShowModal(true);
-    }
-  }, []);
+ 
 
   if (router.pathname.includes("/auth/signup")) return children;
   if (router.pathname.includes("/auth/signin")) return children;
@@ -49,11 +32,7 @@ const Layout = ({ children }: any) => {
   return (
     <div className="w-screen">
       <ToastContainer />
-      <VerifyEmailModal
-        isVisible={showModal}
-        onClose={async () => setShowModal(false)}
-        email={data?.merchant?.email}
-      />
+     
       {!showmerchantLayout && (
         <div>
           <Navbar />
